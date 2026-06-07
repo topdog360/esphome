5c01341f0a39ad39d10ab401795b34fcbe5452d5
Aerith Butler <61718056+jonahnm@users.noreply.github.com>
2026-06-07 12:29:51 -0400

    Fix trait

--- esphome/components/midea_xye/air_conditioner.cpp
+++ esphome/components/midea_xye/air_conditioner.cpp
@@ -445,7 +445,7 @@ float AirConditioner::CalculateTemp(uint8_t byte) {
 
 ClimateTraits AirConditioner::traits() {
   auto traits = ClimateTraits();
-  traits.add_feature_flags(climate::CLIMATE_FEATURE_CURRENT_TEMPERATURE);
+  traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
   traits.set_visual_min_temperature(17);
   traits.set_visual_max_temperature(30);
   traits.set_visual_temperature_step(1.0);
